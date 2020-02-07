22594adcc57517eef81a7d3d5f8b8c4f90a1e634
Sergey <54858700+siruollo@users.noreply.github.com>
2020-02-08 00:06:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//Задача №1
+
+let algebra = 5;
+let georgaphy = 5;
+let physics = 4;
+
+let avg = (algebra + georgaphy + physics) / 3;
+
+console.log(avg);
+
+//Задача №2
+
+let myName = 'Сергей';
+
+console.log(`Привет, мир! Меня зовут ${myName}.`);
+
+//Задача №3
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = (x * y) + (5 * z) + x - 1;
+
+console.log(result);
